8f106003f2045f5ae19d23fde605a285edf0fff0
Ignacio Calderon <ignacio@backbase.com>
2017-11-09 10:15:44 +0100

    chore: pump version 1.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='openstep_parser',
     author='Ignacio Calderon',
     description='OpenStep plist reader into python objects',
     url="http://github.com/kronenthaler/openstep-parser",
-    version='1.3.0',
+    version='1.3.1',
     install_requires=['future'],
     license='BSD License',
     packages=find_packages(exclude=['tests']))
