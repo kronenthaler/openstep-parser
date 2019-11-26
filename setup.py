203ddf218cd093b5520169602b6122bd87f695aa
kronenthaler <kronenthaler@gmail.com>
2019-11-26 13:48:40 +0100

    chore: release 1.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='openstep_parser',
     author='Ignacio Calderon',
     description='OpenStep plist reader into python objects',
     url="http://github.com/kronenthaler/openstep-parser",
-    version='1.3.1',
+    version='1.4.0',
     install_requires=['future'],
     license='BSD License',
     packages=find_packages(exclude=['tests']))
