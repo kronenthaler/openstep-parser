f617aefa7e5b57e571e34321051fad9d9283673d
kronenthaler <kronenthaler@gmail.com>
2020-01-13 13:51:43 +0100

    refactor: remove python 2 compatibility bits

--- openstep_parser/__init__.py
+++ openstep_parser/__init__.py
@@ -1,8 +1,3 @@
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
-from builtins import *
 from .openstep_parser import OpenStepDecoder
+
+__version__ = '1.5.1'
--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 from setuptools import setup, find_packages
 
 
-setup(name='openstep_parser',
+def find_version(*file_paths):
+    def read(*parts):
+        import codecs
+        import os
+        here = os.path.abspath(os.path.dirname(__file__))
+        with codecs.open(os.path.join(here, *parts), 'r') as fp:
+            return fp.read()
+
+    import re
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='openstep_parser',
     author='Ignacio Calderon',
     description='OpenStep plist reader into python objects',
     url="http://github.com/kronenthaler/openstep-parser",
-    version='1.4.0',
-    install_requires=['future'],
+    version=find_version("openstep_parser", "__init__.py"),
     license='BSD License',
-    packages=find_packages(exclude=['tests']))
+    packages=find_packages(exclude=['tests'])
+)
