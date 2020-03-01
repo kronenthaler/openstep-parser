b4ab17981012cf48bda85be65b51c220f11a263a
kronenthaler <kronenthaler@gmail.com>
2020-03-01 14:36:46 +0100

    feat: add support for github actions

--- setup.py
+++ setup.py
@@ -1,4 +1,34 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import os
+
+# Inspired by the example at https://pytest.org/latest/goodpractises.html
+class NoseTestCommand(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Run nose ensuring that argv simulates running nosetests directly
+        import nose
+        nose.run_exit(argv=['nosetests'])
+
+
+class NoseTestCoverage(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Run nose ensuring that argv simulates running nosetests directly
+        import nose
+        nose.run_exit(argv=['nosetests',
+                            '--with-coverage',
+                            '--cover-erase',
+                            '--cover-branches',
+                            '--cover-package=openstep_parser'])
 
 
 def find_version(*file_paths):
@@ -25,5 +55,7 @@ setup(
     url="http://github.com/kronenthaler/openstep-parser",
     version=find_version("openstep_parser", "__init__.py"),
     license='BSD License',
-    packages=find_packages(exclude=['tests'])
+    packages=find_packages(exclude=['tests']),
+    setup_requires=['nose', 'coverage'],
+    cmdclass={'test': NoseTestCommand, 'coverage': NoseTestCoverage}
 )
