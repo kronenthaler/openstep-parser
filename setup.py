d5c44545d45a3d33b00d977bdf6631ea364a2308
kronenthaler <kronenthaler@gmail.com>
2020-03-03 22:28:12 +0100

    refactor: fix coverage report

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ class NoseTestCoverage(TestCommand):
         import nose
         nose.run_exit(argv=['nosetests',
                             '--with-coverage',
+                            '--cover-xml',
                             '--cover-erase',
                             '--cover-branches',
                             '--cover-package=openstep_parser'])
